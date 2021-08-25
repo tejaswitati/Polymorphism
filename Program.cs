74fcd55be6f2bb92ec7f94efe9ec55fbb4f8b696
tejaswi <tejaswitati@gmail.com>
2021-08-25 17:13:47 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public Employee(string firstName, string lastName)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+}
+
+class Developer:Employee
+{
+    string _skills;
+    public Developer(string firstName, string lastName, string skills):base(firstName, lastName)
+    {
+        _skills = skills;
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Developer d = new Developer("Sowmya", "V", "C++");
+    }
+}
